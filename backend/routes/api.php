d44824b8f3ec72f770a03e42e5723fa7e4485f95
Henrique Farias <a.henriquefarias96@gmail.com>
2026-08-01 15:44:44 -0300

    first commit

---
+++ backend/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/users', function () {
+    return User::all();
+});
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
