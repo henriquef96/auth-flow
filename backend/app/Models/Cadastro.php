246d8ed94bd9ddd0f7d4549797ed23c8babfb5db
Henrique Farias <a.henriquefarias96@gmail.com>
2026-08-03 12:37:51 -0300

    Model para gerenciar dados de cadastro

---
+++ backend/app/Models/Cadastro.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cadastro extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nome', 'email', 'cep', 'logradouro',
+        'numero', 'complemento', 'bairro', 'cidade', 'uf'
+    ];
+}
