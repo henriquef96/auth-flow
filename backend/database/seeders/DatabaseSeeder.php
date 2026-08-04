58b790122a811ca5183218fed41d7c60084b1dc9
Henrique Farias <a.henriquefarias96@gmail.com>
2026-08-04 10:17:46 -0300

    Configura o seeder para criar usuário admin padrão

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,12 +3,20 @@
 namespace Database\Seeders;
 
 use App\Models\Cadastro;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
         Cadastro::factory()->count(10)->create();
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@authflow.test',
+            'password' => Hash::make('admin@test'),
+        ]);
+
     }
 }
